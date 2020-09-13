d0ae3aa9896f925bd3437da8013a759bafea5333
prokhorenkovatv <prokhorenkovatv@webmil.eu>
2020-09-13 15:09:09 +0300

    Add description and image in Barcode modal

--- src/services/components/BarcodeItem.js
+++ src/services/components/BarcodeItem.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { StyleSheet, TouchableOpacity, View, TouchableWithoutFeedback } from "react-native";
+import { StyleSheet, TouchableOpacity, View, TouchableWithoutFeedback, Image } from "react-native";
 import { Text } from "native-base";
 import T from "prop-types";
 import Barcode from "react-native-barcode-expo";
@@ -8,6 +8,7 @@ import { colors } from "../../constants";
 import { FavoritesIcon, FavoritesIconOutLine } from "../../assets/svg";
 import { BlurView } from "expo-blur";
 import { UserContext } from "../../reducers/context";
+import { url } from "../../constants";
 
 const s = StyleSheet.create({
   modalContainer: {
@@ -46,11 +47,30 @@ const s = StyleSheet.create({
     fontSize: 20,
     fontWeight: "500"
   },
+  descriptionContainer: {
+    paddingHorizontal: 25,
+    paddingBottom: 10
+  },
+  descriptionText: {
+    fontSize: 14,
+    textAlign: "justify"
+  },
   leftItem: {
     width: 10
   },
   flexMax: {
     flex: 1
+  },
+  imageCompanyContainer: {
+    height: 94,
+    width: 94,
+    justifyContent: "center",
+    marginBottom: 10
+  },
+  image: {
+    height: 94,
+    resizeMode: "contain",
+    width: 94
   }
 });
 
@@ -59,7 +79,7 @@ export default function BarcodeItem({ id, hideModal, handleFavoriteChange }) {
     state: { items }
   } = useContext(UserContext);
 
-  const { fields = {}, is_favorite = false } = items.data.find(item => item.id === id) || {};
+  const { fields = {}, is_favorite = false, image = {} } = items.data.find(item => item.id === id) || {};
   return (
     <BlurView style={s.flexMax} tint="dark" intensity={100}>
       <TouchableOpacity style={s.modalContainer} onPress={hideModal} activeOpacity={1}>
@@ -75,6 +95,16 @@ export default function BarcodeItem({ id, hideModal, handleFavoriteChange }) {
               <TouchableOpacity onPress={handleFavoriteChange}>
                 {is_favorite ? <FavoritesIcon fill={colors.mainRed} /> : <FavoritesIconOutLine />}
               </TouchableOpacity>
+            </View>
+            <View style={s.imageCompanyContainer}>
+              <Image
+                source={{ uri: `${url}${image.preview_list.url}` }}
+                resizeMode={"contain"}
+                style={s.image}
+              />
+            </View>
+            <View style={s.descriptionContainer}>
+              <Text style={s.descriptionText}>{fields.description}</Text>
             </View>
             <Barcode value={`${fields.card_number}`} format="CODE128" />
             <Text>{fields.card_number}</Text>
